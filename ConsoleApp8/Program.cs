7962ca1b9fb210179989b482212e282b77df647d
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-06-30 18:24:07 +0300

    mongodb'den veri cekme ve veri ekleme islemi

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver.Core.Configuration;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+string connectionString = "mongodb://localhost:27017";
+
+var client = new MongoClient(connectionString);
+
+
+var database = client.GetDatabase("local");
+var collection = database.GetCollection<BsonDocument>("books");
+
+Insert();
+
+var filter = Builders<BsonDocument>.Filter.Empty; // Boş bir filtre kullanarak tüm belgeleri çekiyoruz
+
+var documents = collection.Find(filter).ToList();
+
+foreach (var document in documents)
+{
+    Console.WriteLine(document[1] + " " + document[2]);
+}
+
+Console.WriteLine(collection);
+
+Console.ReadLine();
+
+
+void Insert()
+{
+
+    var document = new BsonDocument
+    {
+        { "title", "sefiller" },
+        { "author", "victor hugo" }
+    };
+
+    collection.InsertOne(document);
+    Console.WriteLine("ekleme gerceklesti...");
+}
+
